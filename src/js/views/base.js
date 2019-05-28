993887e2c56c76696b39051b181a011df7ef5d35
rmartinez1 <ronaldbmartinez95@gmail.com>
2019-05-28 18:50:27 -0400

    Recipes now shows up in results list

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,6 @@
 // Here we will have all the elements from the DOM
 export const elements = {
     searchForm: document.querySelector('.search'),
-    searchInput: document.querySelector('.search__field')
+    searchInput: document.querySelector('.search__field'),
+    searchResList: document.querySelector('.results__list')
 };
