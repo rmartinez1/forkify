053968c3036c6a7a2f41e7ef360beb456120a793
rmartinez1 <ronaldbmartinez95@gmail.com>
2019-05-28 23:37:53 -0400

    Page buttons added at the bottom of recipes results

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import Search from './models/Search';
-import * as searchView from './views/SearchView';
+import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
 /*
 Global state of the app
@@ -35,4 +35,13 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
 });
